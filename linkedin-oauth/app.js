b6ef1114757ab58f0c81b8f5fb48d3fb61afdb3b
Matthew Bouchard <bouchard.matthewj@gmail.com>
2016-03-31 16:29:36 -0600

    login logout working

--- linkedin-oauth/app.js
+++ linkedin-oauth/app.js
@@ -9,6 +9,8 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 var passport = require('passport');
+var cookieSession = require('cookie-session');
+var logout = require('express-passport-logout');
 var app = express();
 
 
@@ -23,6 +25,12 @@ app.set('view engine', 'hbs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(passport.initialize());
+
+
+
+
+
+
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
@@ -30,30 +38,38 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(user, done) {
   done(null, user)
 });
-app.use(passport.session());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+    name:'session',
+    keys: [
+        process.env.SECRETKEY_1,
+        process.env.SECRETKEY_2,
+    ]
+}));
 
 passport.use(new LinkedInStrategy({
   clientID: process.env.LINKEDIN_CLIENT_ID,
   clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
   callbackURL: process.env.HOST + "/auth/linkedin/callback",
   scope: ['r_emailaddress', 'r_basicprofile'],
+  state: true
 }, function(accessToken, refreshToken, profile, done) {
   // asynchronous verification, for effect...
-  process.nextTick(function () {
+  // process.nextTick(function () {
     // To keep the example simple, the user's LinkedIn profile is returned to
     // represent the logged-in user. In a typical application, you would want
     // to associate the LinkedIn account with a user record in your database,
     // and return that user instead.
-    return done(null, profile);
-  });
+    console.log('id: ' + profile.id, 'displayName: ' + profile.displayName);
+    return done(null,{id: profile.id, displayName: profile.displayName});
+  // });
 }));
 app.get('/auth/linkedin',
-  passport.authenticate('linkedin', { state: 'SOME STATE'  }),
+  passport.authenticate('linkedin'),
   function(req, res){
     // The request will be redirected to LinkedIn for authentication, so this
     // function will not be called.
@@ -63,6 +79,14 @@ app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
   failureRedirect: '/login'
 }));
 
+app.use(function (req, res, next) {
+  if (!req.session.passport) {
+    app.locals.user = null;
+  }else {
+    app.locals.user = req.session.passport.user;
+  }
+  next()
+})
 
 app.use('/', routes);
 app.use('/users', users);
